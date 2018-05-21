60104d5eee5f2318cd0654cd990e51a5fbcbd03a
Bjay Watanabe Kamwa <bjay.kamwa-watanabe@snips.ai>
2018-05-21 13:39:02 +0000

    add signature on all picture

--- action-printer.py
+++ action-printer.py
@@ -34,51 +34,45 @@ class Skill:
             self.dir[l] = tmp
         print(self.image)
         print(self.dir)
-        self.queue = Queue.Queue()
-
-        self.thread_handler = ThreadHandler()
-        self.thread_handler.run(target=self.start_blocking)
-        self.thread_handler.start_run_loop()
     
-    def start_blocking(self, run_event):
-        while run_event.is_set():
-            try:
-                self.queue.get(False)
-            except Queue.Empty:
-                with Hermes(MQTT_ADDR) as h:
-                    h.subscribe_intents(self.callback).start()
+def extract_image_name(intent_message):
+    image = []
+    if intent_message.slots.image is not None:
+        for room in intent_message.slots.image:
+            image.append(room.slot_value.value.value)
+    return image
 
-    def extract_image_name(self, intent_message):
-        image = []
-        if intent_message.slots.image is not None:
-            for room in intent_message.slots.image:
-                image.append(room.slot_value.value.value)
-        return image
-
-    def callback(self, hermes, intent_message):
-        hermes.publish_end_session(intent_message.session_id, None)
-        image = self.extract_image_name(intent_message)
-        print(image)
-        to_print = ""
-        for tmp in image:
-            for d in self.dir:
-                    if tmp in d:
-                        to_print = DIR + d + "/" + random.choice(self.dir[tmp])
-            for d in self.image:
+def callback(hermes, intent_message):
+    print(intent_message.session_id)
+    image = extract_image_name(intent_message)
+    print(image)
+    to_print = ""
+    for tmp in image:
+        for d in skill.dir:
                 if tmp in d:
-                    to_print = DIR + d
+                    to_print = DIR + d + "/" + random.choice(skill.dir[tmp])
+        for d in skill.image:
+            if tmp in d:
+                to_print = DIR + d
 
-        if to_print == "":
-            for i in image:
-                self.printer.println("We do not have an image of " + i + " in this printer.")
-        else:
-            img = Image.open(to_print)
-            new_width  = 384
-            new_height = new_width * img.size[0] / img.size[1]
-            img = img.resize((new_width, new_height), Image.ANTIALIAS)
-            self.printer.printImage(img, True)
-        self.printer.feed(3)
-        print(image)
+    if to_print == "":
+        return
+        for i in image:
+            skill.printer.println("We do not have an image of " + i + " in this printer.")
+    else:
+        img = Image.open(to_print)
+        new_width  = 384
+        new_height = new_width * img.size[0] / img.size[1]
+        img = img.resize((new_width, new_height), Image.ANTIALIAS)
+        skill.printer.printImage(img, True)
+        skill.printer.println("Printed with Snips.ai")
+    skill.printer.feed(3)
+    print(image)
 
 if __name__ == "__main__":
-    Skill()
+    skill = Skill()
+    
+    with Hermes(MQTT_ADDR) as h: 
+        h.skill = skill
+        h.subscribe_intent("akaisuisei:print", callback) \
+         .loop_forever()
